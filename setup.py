67949d954d44044ea87d2f3f25837daeadcaaf6f
Panu Tulimäki <panu@shuup.com>
2016-08-14 21:41:51 +0300

    Bump version to 1.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 if __name__ == '__main__':
     setuptools.setup(
         name="shuup_megastore_theme",
-        version="1.0.2",
+        version="1.0.3",
         description="Shuup Megastore Theme",
         packages=setuptools.find_packages(),
         include_package_data=True,
