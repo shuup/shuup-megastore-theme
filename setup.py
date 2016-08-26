32ad8bff388255262c76d9ac14fea88347392720
Panu Tulimäki <panu@shuup.com>
2016-08-26 10:04:42 +0300

    Update theme to 1.0.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 if __name__ == '__main__':
     setuptools.setup(
         name="shuup_megastore_theme",
-        version="1.0.5",
+        version="1.0.6",
         description="Shuup Megastore Theme",
         packages=setuptools.find_packages(),
         include_package_data=True,
