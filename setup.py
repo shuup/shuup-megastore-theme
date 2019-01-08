658f357bb2daddf7863619804c0d46be0903dd18
Panu Tulimäki <panu@shuup.com>
2019-01-08 13:42:33 -0800

    Update for latest 1.8+ Shuups

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import setuptools
 
-
 try:
     import shuup_setup_utils
 except ImportError:
@@ -9,14 +8,6 @@ except ImportError:
 
 if __name__ == '__main__':
     setuptools.setup(
-        name="shuup_megastore_theme",
-        version="1.1.3",
-        description="Shuup Megastore Theme",
-        packages=setuptools.find_packages(),
-        include_package_data=True,
-        entry_points={"shuup.addon": "shuup_megastore_theme=shuup_megastore_theme"},
         cmdclass=(shuup_setup_utils.COMMANDS if shuup_setup_utils else {}),
-        install_requires=[
-            'shuup>=0.4',
-        ],
-    )
+        setup_requires=['setuptools>=34.0', 'setuptools-gitver'],
+        gitver=True)
